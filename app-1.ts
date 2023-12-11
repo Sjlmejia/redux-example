c9d534d31338918a87b7feb088fef8466840aede
Jorge Mejia <jorgemejia2304@gmail.com>
2023-12-11 10:12:12 -0500

    first commit

---
+++ app-1.ts
@@ -0,0 +1,50 @@
+
+
+//Acciones
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+function reducer( state = 10, action:Action ) {
+  switch(action.type){
+    case 'INCREMENTAR':
+      return state += 1;
+
+    case 'DECREMENTAR':
+      return state -= 1;
+
+    case 'MULTIPLICAR':
+      return state * action.payload;
+
+    case 'DIVIDIR':
+      return state / action.payload;
+    
+    default:
+      return state;
+  }
+}
+
+//Usar el reducer
+const incrementadorAction: Action = {
+  type: 'INCREMENTAR'
+
+};
+
+console.log(reducer(10, incrementadorAction));
+
+const decrementadorAction: Action = {
+  type: 'DECREMENTAR'
+};
+console.log(reducer(10, decrementadorAction));
+
+const multiplacarAction: Action = {
+  type: 'MULTIPLICAR',
+  payload: 2
+};
+console.log(reducer(10, multiplacarAction));
+
+const dividirAction: Action = {
+  type: 'DIVIDIR',
+  payload: 2
+};
---
+++ app-2.ts
@@ -0,0 +1,31 @@
+import { resetAction, decrementadorAction, dividirAction, incrementadorAction, multiplacarAction } from './contador/contador.actions';
+import { Action } from './ngrx-fake/ngrx';
+
+function reducer( state = 10, action:Action ) {
+  switch(action.type){
+    case 'INCREMENTAR':
+      return state += 1;
+
+    case 'DECREMENTAR':
+      return state -= 1;
+
+    case 'MULTIPLICAR':
+      return state * action.payload;
+
+    case 'DIVIDIR':
+      return state / action.payload;
+    
+    case 'RESET':
+      return state = 0;
+    default:
+      return state;
+  }
+}
+
+
+console.log(reducer(10, incrementadorAction));
+console.log(reducer(10, decrementadorAction));
+console.log(reducer(10, multiplacarAction));
+console.log(reducer(10, dividirAction));
+console.log(reducer(10, resetAction));
+
